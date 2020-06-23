2eec3573b8ff607d6ba427fa478b080e6f9e01d1
Phillip Jones <ptj92e@gmail.com>
2020-06-23 09:07:43 -0500

    Finished Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./pages/About";
@@ -10,13 +9,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Landing />
-        <Navbar />
-        <About />
-        <Projects />
-        <Contact />
-      </Router>
+      <Landing />
+      <Navbar />
+      <About />
+      <Projects />
+      <Contact />
     </div>
   );
 }
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,8 +4,16 @@ import "./css/Landing.css";
 function Landing() {
     return(
         <div id="landing">
-            <h1>I love my wife!</h1>
-            <a href="#about">About</a>
+            <div id="backgroundOverlay">
+                <div id="landingContent">
+                    <div id="topText">
+                        <h1>Hi, I am &nbsp;
+                        <h1 id="name">Phillip Jones</h1>.</h1>
+                    </div>
+                    <h1 id="bottomText">I am a full stack web developer.</h1>
+                    <a href="#about">See What I've Done &nbsp; <i className="fas fa-arrow-right"></i></a>
+                </div>
+            </div>
         </div>
     )
 }
