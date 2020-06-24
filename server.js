020ce8912c5f4c98fa8fcef635ee10331d7aaa2a
Phillip Jones <ptj92e@gmail.com>
2020-06-24 17:24:13 -0500

    Added static content

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const path = require("path");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
